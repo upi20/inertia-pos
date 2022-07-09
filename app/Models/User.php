5231fc179142f842627aa095558113807ee03d65
Isep Lutpi Nur <Iseplutpinur7@gmail.com>
2022-07-09 19:13:21 +0700

    Installasi dan Konfigurasi Laravel Fortify

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes'
     ];
 
     /**
