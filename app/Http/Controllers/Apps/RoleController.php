02165527cdfcf56cac234fd28719d5dc315ccd8b
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2022-11-08 01:32:03 +0700

    add crud Roles

---
+++ app/Http/Controllers/Apps/RoleController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers\Apps;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use App\Http\Controllers\Controller;
+use Spatie\Permission\Models\Permission;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //get roles
+        $roles = Role::when(request()->q, function ($roles) {
+            $roles = $roles->where('name', 'like', '%' . request()->q . '%');
+        })->with('permissions')->latest()->paginate(5);
+
+        //render with inertia
+        return inertia('Apps/Roles/Index', [
+            'roles' => $roles,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //get permission all
+        $permissions = Permission::all();
+
+        //render with inertia
+        return inertia('Apps/Roles/Create', [
+            'permissions' => $permissions,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        /**
+         * Validate request
+         */
+        $this->validate($request, [
+            'name'          => 'required',
+            'permissions'   => 'required',
+        ]);
+
+        //create role
+        $role = Role::create(['name' => $request->name]);
+
+        //assign permissions to role
+        $role->givePermissionTo($request->permissions);
+
+        //redirect
+        return redirect()->route('apps.roles.index');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //get role
+        $role = Role::with('permissions')->findOrFail($id);
+
+        //get permission all
+        $permissions = Permission::all();
+
+        //render with inertia
+        return inertia('Apps/Roles/Edit', [
+            'role'          => $role,
+            'permissions'   => $permissions,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Role $role)
+    {
+        /**
+         * validate request
+         */
+        $this->validate($request, [
+            'name'          => 'required',
+            'permissions'   => 'required',
+        ]);
+
+        //update role
+        $role->update(['name' => $request->name]);
+
+        //sync permissions
+        $role->syncPermissions($request->permissions);
+
+        //redirect
+        return redirect()->route('apps.roles.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //find role by ID
+        $role = Role::findOrFail($id);
+
+        //delete role
+        $role->delete();
+
+        //redirect
+        return redirect()->route('apps.roles.index');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -30,5 +30,9 @@ Route::prefix('apps')->group(function () {
         //route permissions
         Route::get('/permissions', \App\Http\Controllers\Apps\PermissionController::class)->name('apps.permissions.index')
             ->middleware('permission:permissions.index');
+
+        //route resource roles
+        Route::resource('/roles', \App\Http\Controllers\Apps\RoleController::class, ['as' => 'apps'])
+            ->middleware('permission:roles.index|roles.create|roles.edit|roles.delete');
     });
 });
